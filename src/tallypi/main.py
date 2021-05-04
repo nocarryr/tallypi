3bb423ae8c35dc9e8b694aa4d6865d20d16319ca
nocarryr <matt@nomadic-recording.com>
2021-05-04 15:20:58 -0500

    Use loop.add_signal_handler for graceful shutdown

--- src/tallypi/main.py
+++ src/tallypi/main.py
@@ -1,17 +1,26 @@
+from loguru import logger
+import signal
 import asyncio
 
 from tallypi.manager import Manager
 
 def main():
+    async def shutdown(sig, loop, mgr):
+        logger.debug(f'Received {sig.name} signal, shutting down..')
+        await mgr.close()
+        loop.stop()
+
     loop = asyncio.get_event_loop()
     mgr = Manager()
     loop.run_until_complete(mgr.open())
+    signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
+    for s in signals:
+        loop.add_signal_handler(
+            s, lambda s=s: asyncio.create_task(shutdown(s, loop, mgr))
+        )
     try:
         loop.run_forever()
-    except KeyboardInterrupt:
-        loop.run_until_complete(mgr.close())
     finally:
-        loop.run_until_complete(mgr.close())
         loop.close()
 
 if __name__ == '__main__':
